4a6a395d5dca0b5584ce33990ce1f0b956b30695
MilesCranmer <miles.cranmer@gmail.com>
2024-05-24 19:04:00 +0100

    wip: first attempt which doesn't work

--- src/DispatchDoctor.jl
+++ src/DispatchDoctor.jl
@@ -3,22 +3,34 @@ module DispatchDoctor
 export @stable
 
 using MacroTools: combinedef, splitdef
-using Test: _inferred
+using Test: Test
 using TestItems: @testitem
 
-macro stable(fdef)
-    fdef = splitdef(esc(fdef))
+macro stable(fex)
+    fdef = splitdef(fex)
 
-    fdef_caller = deepcopy(fdef)
+    # We create a second f with the actual body
+    fdef2 = splitdef(fex)
+    fdef2_name = gensym("f2")
+    fdef2_args = fdef2[:args]
+    fdef2_kwargs = fdef2[:kwargs]
+    fdef2_body = :($(fdef2_name)($(fdef2_args)...; $(fdef2_kwargs)...))
 
-    fdef[:body] = _inferred(fdef[:body], __module__)
+    fdef2[:name] = fdef2_name
+    
+    # And call this from the primary function, with the _inferred
+    fdef[:body] = Test._inferred(fdef2_body, __module__)
 
-    return combinedef(fdef)
+    return quote
+        $(combinedef(fdef2))
+        $(combinedef(fdef))
+    end
 end
 
 @testitem "stable" begin
     using MacroTools: splitdef
-    @show splitdef(:(f(x) = x))
+
+    @show splitdef(:(f(x::Int; a, b, c::Float32=1., kws...) = x))
 end
 
 end
