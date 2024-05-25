7bf84a6d2b8a01d43c759648259be276579c6a40
MilesCranmer <miles.cranmer@gmail.com>
2024-05-25 12:57:16 +0100

    add missing coverage file

---
+++ coverage.jl
@@ -0,0 +1,20 @@
+using Coverage
+# process '*.cov' files
+coverage = process_folder() # defaults to src/; alternatively, supply the folder name as argument
+# push!(coverage, process_folder("ext")...)
+
+LCOV.writefile("lcov.info", coverage)
+
+# process '*.info' files
+coverage = merge_coverage_counts(
+    coverage,
+    filter!(
+        let prefixes = (joinpath(pwd(), "src", ""), #=joinpath(pwd(), "ext", "")=#)
+            c -> any(p -> startswith(c.filename, p), prefixes)
+        end,
+        LCOV.readfolder("test"),
+    ),
+)
+# Get total coverage for all Julia files
+covered_lines, total_lines = get_summary(coverage)
+@show covered_lines, total_lines
