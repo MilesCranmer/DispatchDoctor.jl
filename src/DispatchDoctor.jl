e3873f51d8f54370b615a755c4d3d228f99805a6
MilesCranmer <miles.cranmer@gmail.com>
2024-05-30 21:08:23 +0100

    customize format

--- src/DispatchDoctor.jl
+++ src/DispatchDoctor.jl
@@ -11,21 +11,16 @@ include("macro_behavior.jl")
 include("stabilization.jl")
 include("macros.jl")
 
+#! format: off
 using ._Utils: extract_symbol, JULIA_OK, Unknown, specializing_typeof
 using ._Errors: TypeInstabilityError, TypeInstabilityWarning, AllowUnstableDataRace
 using ._Preferences
 using ._Printing
-using ._MacroBehavior:
-    MACRO_BEHAVIOR,
-    MacroBehavior,
-    CompatibleMacro,
-    IncompatibleMacro,
-    DontPropagateMacro,
-    register_macro!,
-    get_macro_behavior
+using ._MacroBehavior: MACRO_BEHAVIOR, MacroBehavior, CompatibleMacro, IncompatibleMacro, DontPropagateMacro, register_macro!, get_macro_behavior
 using ._RuntimeChecks: INSTABILITY_CHECK_ENABLED, allow_unstable, is_precompiling
 using ._Stabilization: _stable, _stabilize_all, _stabilize_fnc, _stabilize_module
 using ._Macros: @stable, @unstable
+#! format: on
 
 using TestItems: @testitem
 
