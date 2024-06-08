82a3c2865817afa0756064a405df854bf1c6e640
Avik Pal <avikpal@mit.edu>
2024-06-08 16:59:40 -0700

    Mark broadcasting as nondiff

--- ext/DispatchDoctorChainRulesCoreExt.jl
+++ ext/DispatchDoctorChainRulesCoreExt.jl
@@ -1,8 +1,11 @@
 module DispatchDoctorChainRulesCoreExt
 
-using ChainRulesCore: @ignore_derivatives
+using ChainRulesCore: @ignore_derivatives, @non_differentiable
 import DispatchDoctor._Errors: show_warning, TypeInstabilityWarning
+import DispatchDoctor._Stabilization: _construct_pairs
 
 show_warning(w::TypeInstabilityWarning, ::Int) = (@ignore_derivatives(@warn w); nothing)
+
+@non_differentiable _construct_pairs(::Any...)
 
 end
