f5f2adb27cdf1eb7ea9e269933af5fff31c8c4d3
MilesCranmer <miles.cranmer@gmail.com>
2024-06-09 01:28:30 +0100

    fix errors in benchmark

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -6,27 +6,23 @@ const SUITE = BenchmarkGroup()
 ex = quote
     f() = rand(Bool) ? 0 : 1.0
     f(x) = x
-
     module A
-        function g(; a, b)
-            return a + b
-        end
+    function g(; a, b)
+        return a + b
+    end
 
-        module B
-            using DispatchDoctor: @unstable
-            @unstable h() = rand(Bool) ? 0 : 1.0
-
-            @unstable begin
-                h(x::Int) = rand(Bool) ? 0 : 1.0
-            end
+    module B
+        $(DispatchDoctor).@unstable begin
+            h() = rand(Bool) ? 0 : 1.0
         end
+    end
     end
 end
 
 for mode in ("error", "warn", "disable")
-    options = Any[:(default_mode=$mode)]
+    options = Any[:(default_mode = $mode)]
     SUITE["_stable"]["mode=$mode"] = @benchmarkable(
-        $(_stable)(ex, options; calling_module=nothing, source_info=nothing),
-        setup=(ex=$ex; options=$options)
+        $_stable(options..., ex; calling_module=nothing, source_info=nothing),
+        setup = (ex = $(QuoteNode(ex)); options = $options)
     )
 end
