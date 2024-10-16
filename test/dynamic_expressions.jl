3fb4767246c33bbf0638d707b0269b077a175706
Miles Cranmer <miles.cranmer@gmail.com>
2024-10-16 07:03:05 +0100

    test: fix compat of DE

--- test/dynamic_expressions.jl
+++ test/dynamic_expressions.jl
@@ -9,17 +9,23 @@ mktempdir() do tempdir
     run(`$(git()) clone $repo_url $dest_dir`)
     run(`$(git()) -C $dest_dir checkout v1.2.0`)
 
+    dd_path = dirname(dirname(@__FILE__))
+    dd_toml_content = TOML.parsefile(
+        joinpath(dd_path, "Project.toml")
+    )
+    dd_version = dd_toml_content["version"]
+
     # Modify compat in Project.toml
     project_toml_path = joinpath(dest_dir, "Project.toml")
     toml_content = TOML.parsefile(project_toml_path)
-    delete!(toml_content["compat"], "DispatchDoctor")
+    toml_content["compat"]["DispatchDoctor"] = "=$dd_version"
     open(project_toml_path, "w") do io
         TOML.print(io, toml_content)
     end
 
     # Activate the environment
     Pkg.activate(dest_dir)
-    Pkg.develop(; path=dirname(dirname(@__FILE__)))
+    Pkg.develop(; path=dd_path)
     Pkg.instantiate()
 
     # Run tests
